2642c0f311ed13128e6ba699f2f1c814af6fdfd8
Pekka Neuvo <pekka.neuvo@futurice.com>
2017-04-26 10:40:40 +0300

    add 404 to client

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const PageNotFound: React.StatelessComponent<undefined> = () => (
+    <div className="page-not-found">
+        404 - page not found
+    </div>
+);
+
+export default PageNotFound;
